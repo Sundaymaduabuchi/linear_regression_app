29e6b92ff4d69a590f201dcc26c0bc046967a404
Sundaymaduabuchi <maduabuchisunday2018@gmail.com>
2026-01-24 12:02:52 +0100

    Built FastAPI app

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import  streamlit as st
+import requests
+import joblib
+import numpy as  np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Load model
+model = joblib.load("model/model.pkl")
 
+# app title and description
+st.title("Power Out Prediction")
+st.write("Enter the values for the following features to predict power output (PE).")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# User input for each feature
+ambient_temperature = st.number_input("Ambient Temperature (AT)", value=15.0)
+exhaust_vacuum = st.number_input("Exhaust Vacuum(V)", value=40.0)
+ambient_pressure = st.number_input("Ambient Pressure(AP)", value=1000.0)
+relative_humidity = st.number_input("Relative Humidity(RH)", value=75.0)
 
+if st.button("predict"):
+    input_data = np.array([[
+        ambient_temperature, exhaust_vacuum, ambient_pressure, relative_humidity
+    ]])
+    prediction = model.predict(input_data)
+    st.write(f"Predicted Power Output (PE): {prediction[0]}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # prepare data
+   # input_data = {
+   #     "ambient_temperature": ambient_temperature,
+   #     "exhaust_vacuum": exhaust_vacuum,
+   #     "ambient_pressure": ambient_pressure,
+   #    "relative_humidity": relative_humidity
+   # }
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # make api call
+   # response = requests.post("http://localhost:8000/predict", json=input_data)
+
+    #if response.status_code == 200:
+    #    prediction = response.json()["Prediction"]
+    #    st.write(f"Predicted Power Output (PE): {prediction}")
+    #else:
+    #    st.write("Error in prediction. Please try again.")
